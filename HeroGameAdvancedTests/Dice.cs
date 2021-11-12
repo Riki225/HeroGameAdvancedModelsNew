eef8bae8611f4635096f0bf13ef47b54ed7a30ad
Riki225 <79812982+Riki225@users.noreply.github.com>
2021-11-12 22:11:18 +1100

    Initial Set Up

---
+++ HeroGameAdvancedTests/Dice.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using HeroGameAdvancedLib;
+
+namespace HeroGameAdvancedTests
+{
+    public class DiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ HeroGameAdvancedTests/VillainTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using HeroGameAdvancedLib;
+
+
+namespace HeroGameAdvancedTests
+{
+    public class VillainTests
+    {
+        Villain villain1;
+
+        public VillainTests(){}
+
+        [Fact]
+        public void Test1()
+        {
+}
+    }
+}
